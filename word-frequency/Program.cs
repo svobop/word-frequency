b9f0dfe0d0711e25feb152a5d87ae2de46c92c99
svobop <petr.svoboda@jumpshot.com>
2020-02-26 17:06:25 +0100

    create list of word and unique words

--- word-frequency/Program.cs
+++ word-frequency/Program.cs
@@ -8,18 +8,26 @@ namespace word_frequency
     {
         public static void getWordAtFrequency(int frequency)
         {
+            string textLines = "";
             try
             {
                 using (StreamReader sr = File.OpenText("input.txt"))
                 {
-
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        textLines = textLines + line;
+                    }
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("input.txt doesn't exist");
+                Console.WriteLine("input.txt wasn't found");
             }
-            
+
+            var listWords = textLines.Split(" ");
+            var uniqueWords = new HashSet<string>(listWords);
+
         }
 
         static void Main(string[] args)
