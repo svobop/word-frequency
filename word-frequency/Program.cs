8099e6dd520cd347bec95e373bcfbf335e423693
svobop <petr.svoboda@jumpshot.com>
2020-02-26 16:50:26 +0100

    Add project files.

---
+++ word-frequency/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace word_frequency
+{
+    class Program
+    {
+        public static void getWordAtFrequency(int frequency)
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText("input.txt"))
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("input.txt doesn't exist");
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            getWordAtFrequency(2);
+        }
+    }
+}
